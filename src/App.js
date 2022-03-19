924d82176f6ea7511bb6500f681595d89df0e70f
khawlames <mesbehi.khawla@yahoo.fr>
2022-03-19 10:52:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import imag from './imageInSrc.jpg.jpg';
+import './style.css';   
 
 function App() {
   return (
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <header className="App-header">
+   
+    <h1 className='title red' style={{ color: "red", fontSize: 60 }}>Mesbehi Khawla</h1>
+    <img className='imagee' src={imag} alt ='imageInSrc'  />
+    <br/>
+    <img className='imgpub'   src="/image/img1.jpg"/>
+
+<br/>
+
+    <video className='vd' width="500" height="300" controls loop autoPlay muted>
+
+<source src="https://civam.fr/wp-content/uploads/sites/4/2014/10/big_buck_bunny.mp4" type="video/mp4" />
+
+</video>
+    </header>
+      </div>);
+      
+  }
+ 
+
+
 
 export default App;
